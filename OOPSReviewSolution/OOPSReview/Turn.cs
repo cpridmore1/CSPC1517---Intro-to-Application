f0b72d174ebb3182e8fca9f1f6047832029febec
cpridmore1 <46543427+cpridmore1@users.noreply.github.com>
2019-01-14 16:54:09 -0700

    Update Turn

--- OOPSReviewSolution/OOPSReview/Turn.cs
+++ OOPSReviewSolution/OOPSReview/Turn.cs
@@ -6,7 +6,53 @@ using System.Threading.Tasks;
 
 namespace OOPSReview
 {
-    class Turn
+    public class Turn
     {
+
+        private int _PlayerOneRoll;
+
+        public int PlayerOneRoll
+        {
+            get
+            {
+                return _PlayerOneRoll;
+            }
+
+            set
+            {
+                _PlayerOneRoll = value;
+            }
+
+        }
+
+        public int PlayerTwoRoll { get; set; }
+
+        public Turn()
+        {
+
+
+        }
+
+        public Turn(int player1, int player2)
+        {
+            PlayerOneRoll = player1;
+            PlayerTwoRoll = player2;
+
+        }
+
+        //methods
+        //public string FindRollResults()
+        //{
+        //   return null;
+        //}
+
+
+
+
+
+
+
+
+
     }
 }
